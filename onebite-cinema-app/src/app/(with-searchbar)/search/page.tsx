d39bec423cf10366530ca397e8c3c66937593a79
Hailey <hailey.soojung@gmail.com>
2025-04-13 12:49:59 +0900

    add day14

--- onebite-cinema-app/src/app/(with-searchbar)/search/page.tsx
+++ onebite-cinema-app/src/app/(with-searchbar)/search/page.tsx
@@ -1,8 +1,10 @@
 import MovieItem from '@/components/movie-item';
 import style from './page.module.css';
 import { MovieData } from '@/types';
+import { delay } from '@/util/delay';
 
 export default async function Page({ searchParams }: { searchParams: Promise<{ q?: string }> }) {
+  await delay(1500);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER}/movie/search?q=${(await searchParams).q}`,
     { cache: 'force-cache' }
